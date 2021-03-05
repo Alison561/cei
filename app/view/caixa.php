e6a9840768b44d67539be910ec10a45c7c94672b
William Sodré <71849007+william0035@users.noreply.github.com>
2021-03-04 22:50:35 -0300

    telas prontas

---
+++ app/view/caixa.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CAIXA</title>
+    <link rel="icon" href="imagens/icone-CEI.png" type="image/png" >
+    <link rel ="stylesheet" href="css/estilocaixa.css">
+</head>
+<body>
+    
+<div class="container">
+<header>
+    <img src="imagens/LOGO-CEI-SEM-FUNDO_200x70.png" alt="logo">
+    <h1>NOME DO SEU ESTABELECIMENTO</h1>    
+    <h3>Olá Usuario</h3>
+</header>
+<div class="box">
+    <div class="maintitle"><h1>CAIXA</h1></div>
+    <div class="item descricao">
+        <div class="titulo"><h2>DESCRIÇÃO</h2></div>
+    </div>
+    <div class="item quantidade">
+        <div class="titulo"><h2>QUANTIDADE</h2></div>
+    </div>
+    <div class="item valor">
+        <div class="titulo"><h2>VALOR</h2></div>
+    </div>
+    <div class="item logo">
+        <h2>SUA LOGO AQUI</h2>
+    </div>
+    <div class="item lista">
+        <div class="titulo"><h2>LISTA DE COMPRAS</h2></div>
+    </div>
+    <div class="item total">
+        <div class="titulo"><h2>TOTAL</h2></div>
+    </div>
+    <div class="item funcao">
+        <button class="excluir">EXCLUIR ITEM - F1</button>
+        <button class="cancelar">CANCELAR COMPRA - F2</button>
+        <button class="finalizar">FINALIZAR COMPRA - F3</button>
+    </div>
+</div>   
+</div>
+</body>
+</html>     
--- app/view/home.php
+++ app/view/home.php
@@ -11,17 +11,16 @@
 
 <div class="container">
     <header class="headerhome">
-    <img src="imagens/LOGO-CEI-SEM-FUNDO_600x324.png" alt="logo_cei" class="logo">  <h3>Olá Usuario</h3>
+    <img src="imagens/LOGO-CEI-SEM-FUNDO_600x324.png" alt="logo_cei" class="logo">  
+    <h3>Olá Usuario</h3>
     </header>
     <div class="box-home">
-        <nav class="nav-home">
             <a href="cadastroUsuario.html"><button class="btnhome btnhome2"><img src="imagens/ICONE ITENS.png"><br>CADASTRAR PRODUTO</button></a>
             <a href="cadastroUsuario.html"><button class="btnhome btnhome1"><img src="imagens/ICONE CAIXA.png"><br>CAIXA</button></a>
             <a href="cadastroUsuario.html"><button class="btnhome btnhome3"><img src="imagens/ICONE USUARIO.png"><br>CADASTRAR USUÁRIO</button></a>
             <a href="cadastroUsuario.html"><button class="btnhome btnhome4"><img src="imagens/ICONE INVENTARIO.png"><br>INVENTÁRIO</button></a>
             <a href="cadastroUsuario.html"><button class="btnhome btnhome5"><img src="imagens/ICONE RELATORIO.png"><br>RELÁTORIOS</button></a>
             <a href="cadastroUsuario.html"><button class="btnhome btnhome6"><img src="imagens/ICONE SAIR.png"><br>SAIR</button></a>
-        </nav>
     </div>
 </div>
 
