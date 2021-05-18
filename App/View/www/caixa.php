dafc369d1dca6cc717375b504abb56d7884b478f
Alison Vitor <71190247+Alison561@users.noreply.github.com>
2021-05-18 20:53:22 -0300

    'subi'

---
+++ App/View/www/caixa.php
@@ -0,0 +1,33 @@
+ <div class="container">
+    <header>
+        <img src="<?= url ?>public/imagens/LOGO-CEI-SEM-FUNDO_200x70.png" alt="logo">
+        <h1>NOME DO SEU ESTABELECIMENTO</h1>    
+        <h3>Olá Usuario</h3>
+    </header>
+    <div class="box">
+        <div class="maintitle"><h1>CAIXA</h1></div>
+        <div class="item descricao">
+            <div class="titulo"><h2>DESCRIÇÃO</h2></div>
+        </div>
+        <div class="item quantidade">
+            <div class="titulo"><h2>QUANTIDADE</h2></div>
+        </div>
+        <div class="item valor">
+            <div class="titulo"><h2>VALOR</h2></div>
+        </div>
+        <div class="item logo">
+            <h2>SUA LOGO AQUI</h2>
+        </div>
+        <div class="item lista">
+            <div class="titulo"><h2>LISTA DE COMPRAS</h2></div>
+        </div>
+        <div class="item total">
+            <div class="titulo"><h2>TOTAL</h2></div>
+        </div>
+        <div class="item funcao">
+            <button class="excluir">EXCLUIR ITEM - F1</button>
+            <button class="cancelar">CANCELAR COMPRA - F2</button>
+            <button class="finalizar">FINALIZAR COMPRA - F3</button>
+        </div>
+    </div>   
+</div>    
