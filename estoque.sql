056a4dc4990ac8dcb2809d90e7ab9f204ac08c76
Alison Vitor <71190247+Alison561@users.noreply.github.com>
2021-06-03 00:43:39 -0300

    final

--- estoque.sql
+++ estoque.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 03-Jun-2021 às 05:12
+-- Tempo de geração: 03-Jun-2021 às 05:43
 -- Versão do servidor: 10.4.17-MariaDB
 -- versão do PHP: 8.0.2
 
@@ -20,6 +20,29 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `estoque`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produto`
+--
+
+CREATE TABLE `produto` (
+  `id` int(11) NOT NULL,
+  `codigo` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `qnt` decimal(10,2) NOT NULL,
+  `preco` decimal(10,2) NOT NULL,
+  `data` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `produto`
+--
+
+INSERT INTO `produto` (`id`, `codigo`, `nome`, `qnt`, `preco`, `data`) VALUES
+(3, 12345, 'macarrao', '9994.00', '3.00', '2021-03-08'),
+(4, 22222, 'feijão', '9.00', '11.11', '2021-05-18');
 
 -- --------------------------------------------------------
 
@@ -81,9 +104,39 @@ INSERT INTO `relatorio` (`id`, `id_prod`, `tipo`, `qntd`, `data`) VALUES
 (71, 4, 'venda', 2, '2021-06-03'),
 (72, 3, 'venda', 6, '2021-06-03');
 
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `nivel` int(11) NOT NULL,
+  `cpf` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `nome`, `senha`, `nivel`, `cpf`) VALUES
+(1, 'santosalison561@gmail.com', 'Alison Vitor', '12345', 0, '000.000.000-00'),
+(4, 'santos@gmail.com', 'Alison Vitor Brito Souza', '12345', 1, '111.111.111-11');
+
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices para tabela `produto`
+--
+ALTER TABLE `produto`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `codigo` (`codigo`);
 
 --
 -- Índices para tabela `relatorio`
@@ -93,14 +146,34 @@ ALTER TABLE `relatorio`
   ADD KEY `id_prod` (`id_prod`);
 
 --
+-- Índices para tabela `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `cpf` (`cpf`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
 -- AUTO_INCREMENT de tabelas despejadas
 --
+
+--
+-- AUTO_INCREMENT de tabela `produto`
+--
+ALTER TABLE `produto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `relatorio`
 --
 ALTER TABLE `relatorio`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
+
+--
+-- AUTO_INCREMENT de tabela `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
